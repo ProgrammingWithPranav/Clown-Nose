66ac040f6b2e30c074c449a736cc8b2d001b0b0d
Prana Tantri <67408817+ProgrammingWithPranav@users.noreply.github.com>
2021-03-09 13:54:17 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+function preload() {
+    img = loadImage("https://i.postimg.cc/7ZBcjDqp/clownnose.png"); 
+}
+
+function setup() {
+    canvas = createCanvas(300, 300)
+    canvas.center()
+}
+
+function draw(){
+
+}
+
+function takeSnapshot() {
+    save('clown.png');
+}
